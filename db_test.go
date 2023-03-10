db8b1111013597a721ffca4a8ab2722c957ba36c
whitesirwang <443532423@qq.com>
2023-03-10 14:23:15 +0800

    fix

--- db_test.go
+++ db_test.go
@@ -19,7 +19,7 @@ import (
 	"time"
 	"unsafe"
 
-	"github.com/boltdb/bolt"
+	"github.com/whitesirwang/wbdb"
 )
 
 var statsFlag = flag.Bool("stats", false, "show performance stats")
--- simulation_test.go
+++ simulation_test.go
@@ -7,7 +7,7 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/boltdb/bolt"
+	"github.com/whitesirwang/wbdb"
 )
 
 func TestSimulate_1op_1p(t *testing.T)     { testSimulate(t, 1, 1) }
